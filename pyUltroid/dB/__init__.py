cca6786a495e6160aca2b3c65def78e8816f47f4
riizzvbss <anandamahar084@gmail.com>
2022-10-21 13:00:44 +0700

    Update __init__.py

--- pyUltroid/dB/__init__.py
+++ pyUltroid/dB/__init__.py
@@ -17,17 +17,18 @@ DEVLIST = [
     611816596,  # @Arnab431
     1318486004,  # @sppidy
     803243487,  # @hellboi_atul
+    1054295664,  # @riizzvbss
 ]
 
 ULTROID_IMAGES = [
     f"https://graph.org/file/{_}.jpg"
     for _ in [
-        "ec250c66268b62ee4ade6",
-        "3c25230ae30d246194eba",
-        "b01715a61b9e876c0d45d",
-        "4ceaf720a96a24527ecff",
-        "a96223b574f29f3f0d184",
-        "6e081d339a01cc6190393",
+        "098f5964288af38f0f343",
+        "098f5964288af38f0f343",
+        "098f5964288af38f0f343",
+        "098f5964288af38f0f343",
+        "098f5964288af38f0f343",
+        "098f5964288af38f0f343",
     ]
 ]
 
