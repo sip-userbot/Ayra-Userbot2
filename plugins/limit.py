5973f3f1a6b6bf7552549f899b83332c359af641
riizzvbss <anandamahar084@gmail.com>
2022-10-22 21:39:12 +0700

    Update limit.py

--- plugins/limit.py
+++ plugins/limit.py
@@ -1,18 +1,31 @@
+# SADBOY
+
+"""
+✘ Commands Available -
+
+• `{i}limit`
+   Periksa Anda terbatas atau tidak!
+"""
+
 from telethon import events
 from telethon.errors.rpcerrorlist import YouBlockedUserError
-from telethon.tl.functions.account import UpdateNotifySettingsRequest
+
+from . import ultroid_cmd
 
 
 @ultroid_cmd(pattern="limit$")
 async def demn(ult):
     chat = "@SpamBot"
-    await ult.edit("Memeriksa Apakah Anda Terbatas...")
+    msg = await ult.eor("Memeriksa Apakah Anda Terbatas...")
     async with ult.client.conversation(chat) as conv:
         try:
-            response = conv.wait_event(events.NewMessage(incoming=True, from_users=178220800))
-            await ult.client.send_message(chat, "/start")
+            response = conv.wait_event(
+                events.NewMessage(incoming=True, from_users=178220800)
+            )
+            await conv.send_message("/start")
             response = await response
+            await ult.client.send_read_acknowledge(chat)
         except YouBlockedUserError:
-            await ult.reply("Silahkan Buka Blokir @SpamBot ")
+            await msg.edit("Silahkan Buka Blokir @SpamBot ")
             return
-        await eor(ult, response.message.message)
+        await msg.edit(f"~ {response.message.message}")
